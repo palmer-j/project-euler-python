6ed64c9ebfec4d1452658b6df222bd1ac7446fa1
Johnny Palmer <johnny.palmer.work@gmail.com>
2023-02-23 18:18:21 +0000

    initial commit

---
+++ problems/p001.py
@@ -0,0 +1,19 @@
+# 
+# Solution to Project Euler problem 1
+# Multiples of 3 or 5
+# 
+
+# If we list all the natural numbers below 10 that are multiples
+# of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+def sum_of_multiples_3_or_5(num):
+	return sum([x for x in range(num) if x % 3 == 0 or x % 5 == 0])
+
+def solve():
+	return str(sum_of_multiples_3_or_5(1000))
+
+if __name__ == "__main__":
+	print(solve())
---
+++ tests/test_p001.py
@@ -0,0 +1,17 @@
+"""Unit tests for p1.py."""
+
+import unittest
+
+from problems.p001 import sum_of_multiples_3_or_5
+
+
+class TestSumOfMultiples(unittest.TestCase):
+
+    def test_sum_of_multiples(self):
+        self.assertEqual(sum_of_multiples_3_or_5(0), 0)
+        self.assertEqual(sum_of_multiples_3_or_5(4), 3)
+        self.assertEqual(sum_of_multiples_3_or_5(6), 8)
+        self.assertEqual(sum_of_multiples_3_or_5(10), 23)
+        self.assertEqual(sum_of_multiples_3_or_5(11), 33)
+        self.assertEqual(sum_of_multiples_3_or_5(12), 33)
+        self.assertEqual(sum_of_multiples_3_or_5(13), 45)
