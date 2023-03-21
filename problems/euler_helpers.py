3f517c2eadbf41b9032eb57e00312cdc76e00f84
Johnny Palmer <johnny.palmer.work@gmail.com>
2023-03-21 14:50:55 +0000

    renmamed euler_helpers & fixed is_prime()

---
+++ problems/euler_helpers.py
@@ -0,0 +1,24 @@
+import math
+
+def is_prime(x):
+    if x <= 1: # by definition negative numbers are not prime
+        return False
+    elif x <= 3:
+        return True
+    elif x % 2 == 0 or x % 3 == 0:
+        return False
+    else:
+        # given that any prime can be expressed in the form  (6k ± 1)
+        # we can optimise the trial division method
+        lim = int(math.sqrt(x))
+        for i in range(5, lim + 1, 6):
+            if x % i == 0 or x % (i + 2) == 0:
+                return False
+        return True
+
+def list_primes(n):
+    pass
+
+def prime_generator(limit):
+    pass
+
