71ff95751c63ff6f1b8550d50d6de9dc5e5c220a
Johnny Palmer <johnny.palmer.work@gmail.com>
2023-04-28 12:37:10 +0100

    fixed p9 unittest

--- tests/test_p009.py
+++ tests/test_p009.py
@@ -8,5 +8,10 @@ from problems.p009 import pythag_trip_with_target
 class TestLargestProduct(unittest.TestCase):
 
     def test_largest_product(self):
-        self.assertEqual(pythag_trip_with_target(12), 60)
-        self.assertEqual(pythag_trip_with_target(1000), 31875000)
+        a, b, c = pythag_trip_with_target(12)
+        prod = a * b * c
+        self.assertEqual(prod, 60)
+
+        a, b, c = pythag_trip_with_target(1000)
+        prod = a * b * c
+        self.assertEqual(prod, 31875000)
